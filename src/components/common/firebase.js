242a40cdae8bbc49d930da466709daf64f0ce3bf
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-09-12 21:09:54 +0530

    update env

--- src/components/common/firebase.js
+++ src/components/common/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebase = initializeApp({
-  apiKey: "AIzaSyC_nuiuglzcd3YMU2dY0SnsfzpNo41i1tw",
-  authDomain: "beyondchats-684da.firebaseapp.com",
-  projectId: "beyondchats-684da",
-  storageBucket: "beyondchats-684da.appspot.com",
-  messagingSenderId: "704952239667",
-  appId: "1:704952239667:web:468b2ad1906483cd0fc546",
-  measurementId: "G-QPKTLKF13Q"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export default firebase;
